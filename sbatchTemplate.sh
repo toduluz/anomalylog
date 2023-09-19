f0e342d52aa4a0621b66b8d8694d9496d7d3b616
toduluz <limyf_343@hotmail.com>
2023-09-19 16:32:02 +0800

    modify sbatchTemplate.sh

--- sbatchTemplate.sh
+++ sbatchTemplate.sh
@@ -9,8 +9,8 @@
 
 #SBATCH --nodes=1                   # How many nodes required? Usually 1
 #SBATCH --cpus-per-task=4           # Number of CPU to request for the job
-#SBATCH --mem=8GB                   # How much memory does your job require?
-#SBATCH --gres=gpu:1                # Do you require GPUS? If not delete this line
+#SBATCH --mem=64GB                   # How much memory does your job require?
+#SBATCH --gres=gpu:4                # Do you require GPUS? If not delete this line
 #SBATCH --time=02-00:00:00          # How long to run the job for? Jobs exceed this time will be terminated
                                     # Format <DD-HH:MM:SS> eg. 5 days 05-00:00:00
                                     # Format <DD-HH:MM:SS> eg. 24 hours 1-00:00:00 or 24:00:00
