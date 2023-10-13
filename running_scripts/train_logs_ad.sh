42d18cabc1f77876ed56a0bfbbe51d91d9e31997
toduluz <limyf_343@hotmail.com>
2023-10-13 23:35:03 +0800

    new commit

--- running_scripts/train_logs_ad.sh
+++ running_scripts/train_logs_ad.sh
@@ -15,7 +15,7 @@ SR=20
 
 # python3 models/hat/convert_roberta_to_htf.py --layout ${LAYOUT} --max_sentences ${MAX_SENTENCES} --max_sentence_length ${MAX_SENTENCE_LENGTH} \
 
-accelerate launch evaluation/run_logs_ad2.py \
+accelerate launch --main_process_port 29500 evaluation/run_logs_ad3.py \
     --model_name_or_path data/PLMs/hat-${LAYOUT}-roberta-${MAX_SENTENCES}-${MAX_SENTENCE_LENGTH}-${MODEL_MAX_LENGTH} \
     --dataset_name data/${DATASET_NAME_2} \
     --output_dir data/PLMs/hat/${LAYOUT}-roberta-${MAX_SENTENCES}-${MAX_SENTENCE_LENGTH}-${MODEL_MAX_LENGTH}/${DATASET_NAME_2}/mlm${MLM}-top${TOP_K}-so-sr${SR}-${LR}-win50\
@@ -33,19 +33,22 @@ accelerate launch evaluation/run_logs_ad2.py \
     --label_names labels primary_labels secondary_labels tertiary_labels \
     --do_predict \
     --logging_strategy steps \
-    --logging_steps .1 \
+    --logging_steps ${STEPS} \
     --per_device_train_batch_size 4 \
     --per_device_eval_batch_size 4 \
-    --gradient_accumulation_steps 4 \
-    --eval_accumulation_steps 4 \
+    --gradient_accumulation_steps 1 \
+    --eval_accumulation_steps 1 \
     --max_seq_length ${MODEL_MAX_LENGTH} \
     --max_sentences ${MAX_SENTENCES} \
     --metric_for_best_model 'auroc' \
     --load_best_model_at_end \
     --overwrite_output_dir \
-    --num_workers 30 \
-    --max_steps 200000 \
-    # --max_eval_samples 32 \
-    # --max_predict_samples 32 \
-    # --max_train_samples 200000 \
+    --num_workers 120 \
+    --mlm ${MLM} \
+    --topk ${TOP_K} \
+    --sr ${SR} \
+    --max_steps 50000 \
+    # --max_train_samples ${SAMPLE} \
+    # --max_eval_samples ${SAMPLE} \
+    # --max_predict_samples ${SAMPLE} \
     # --num_train_epochs 10 \
